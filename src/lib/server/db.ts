fe42dcdabd36067421d6acd845d239d7e4022942
RayRaivern <ray456654@gmail.com>
2026-05-04 15:19:44 +0600

    Changed database connection to pool connection, since that's better.

--- src/lib/server/db.ts
+++ src/lib/server/db.ts
@@ -1,9 +1,12 @@
 import mysql from 'mysql2/promise';
 import { DB_HOST, DB_USER, DB_PASSWORD, DB_NAME } from '$env/static/private';
 
-export const db = await mysql.createConnection({
-	host: DB_HOST,
-	user: DB_USER,
-	password: DB_PASSWORD,
-	database: DB_NAME
+export const db = mysql.createPool({
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
