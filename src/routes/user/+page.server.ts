b5f26925767cc2b115bcdbe9cf64481787285540
Zarif Yeameen <114343857+rzyzarific@users.noreply.github.com>
2026-05-04 14:54:03 +0600

    Update borrow history

--- src/routes/user/+page.server.ts
+++ src/routes/user/+page.server.ts
@@ -1,8 +1,29 @@
+import { db } from '$lib/server/db';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = ({ locals }) => {
-    // If we reached this point, the hook has already guaranteed locals.user exists
+export const load: PageServerLoad = async ({ locals }) => {
+
+    const userId = locals.user.id;
+
+    const [historyRows]: any = await db.query(
+        `SELECT 
+            l.loan_id,
+            l.borrow_date,
+            l.due_date,
+            l.return_date,
+            b.name AS title,
+            b.author,
+            c.barcode
+        FROM Loans l
+        JOIN Copy c ON l.barcode = c.barcode
+        JOIN Book b ON c.ISBN = b.ISBN
+        WHERE l.user_id = ?
+        ORDER BY l.borrow_date DESC`,
+        [userId]
+    );
+
     return {
-        user: locals.user
+        user: locals.user,
+        history: historyRows
     };
 };
